3ddb6dc38b053baf9d750a2377d5a8a4cbb069a3
xianyang <xianyang.lu@vcg.com>
2024-08-21 11:18:13 +0800

    i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 
 // languages supported
-export const locales = ['en', 'fr', 'de', 'es'] as const;
+export const locales = ['en', 'fr', 'de', 'es', 'it', 'pt', 'nl', 'sv', 'da', 'no', 'fi', 'ro', 'pl'] as const;
 
 export type Locale = (typeof locales)[number]
 export const defaultLocale = 'en'
